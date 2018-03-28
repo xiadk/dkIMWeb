0cbace23eaeba68178913665dadd6ab73e1ac189
xiadk <13047901299@139.com>
2018-03-29 00:30:51 +0800

    实现三个界面动态切换

--- webroot/js/dkjs/main.js
+++ webroot/js/dkjs/main.js
@@ -30,9 +30,11 @@ function addFriend() {
             $("#afv-close").show();
             $("#search-result-list-tab").show();
             var friends = msg.friends;
-            
-            alert(friends[0].name);
+            getFriend(friends);
         });
+    });
+    $("#afv-close").click(function () {
+        $("#afv-close").hide();
     });
 }
 
@@ -52,26 +54,24 @@ function getFriend($friends) {
         $("#search-result-container").append("<div class='srl-item clear' data-account='11'><div class='srl-item-avatar'><img src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=587180231,3004218230&fm=27&gp=0.jpg' alt='真善美'></div><div class='srl-item-text-container'><div class='srl-item-nickname'>真善美</div><div class='srl-item-mtnum'>mt_zsm</div></div><div class='srl-item-add-btn'></div></div>")
 
     }
-
 }
 
 $(".tab").click(function(event){
-    var index=0;
-    $.each($(".tab"),function(i,n){
-        $(n).removeClass("cur");
-        if($(this).attr("id") == $(n).attr("id")){
-            index = i;
-        }
-    });
-
+    $(".tab").removeClass("cur");
+    $(".tab-view-container").addClass("hide");
+    $(".chat--layout").addClass("hide");
     $(this).addClass("cur");
-
+    var data_id_val = $(this).attr("data-id");
     $.each($(".tab-view-container"),function(i,n){
-        if(i==index){
-            $(n).addClass("cur");
+         var childVal = $(n).children().attr("data-id");
+        if(data_id_val==childVal){
+            $(n).removeClass("hide");
         }
-        $(n).removeClass("cur");
     });
-
-
+    $.each($(".chat--layout"),function(i,n){
+         var chat_id = $(n).attr("data-id");
+        if(data_id_val==chat_id){
+            $(n).removeClass("hide");
+        }
+    });
 });
