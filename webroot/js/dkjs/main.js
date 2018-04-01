e71add78ac289d1b8fd41425270ff930adaf8190
xiadk <13047901299@139.com>
2018-04-02 07:11:32 +0800

    发送好友请求成功，准备接受好友请求

--- webroot/js/dkjs/main.js
+++ webroot/js/dkjs/main.js
@@ -45,7 +45,10 @@ function addFriend() {
         var token = readCookie("token");
         var message = new form(token,OPE_PERSONAL,fid,TYPE_ADD_FRIEND,"");
         wsSend(message);
+        $("#apply-friend-verify-tab").hide();
+        $("#usual-alert-tab").show();
     });
+    $("#ua-submit").hide();
 }
 
 
@@ -65,19 +68,18 @@ function getFriend($friends) {
         var imgUrl = $friends[i].photo;
         var name = $friends[i].name;
         var phone = $friends[i].phone;
-        var id = $friends[i].id;
+        var id = $friends[i].uid;
         $("#search-result-container").empty();
         $("#search-result-container").append("<div class='srl-item clear' data-account='" + id + "'><div class='srl-item-avatar'><img src='" + imgUrl + "' alt='"+name+"'></div><div class='srl-item-text-container'><div class='srl-item-nickname'>" + name + "</div><div class='srl-item-mtnum'>" + phone + "</div></div><div class='srl-item-add-btn'></div></div>")
 
     }
     $(".srl-item-add-btn").click(function () {
         $("#search-result-list-tab").hide();
-        var photo = $(this).parent().children("img").attr("src");
-        var name = $(this).parent().children("img").attr("alt");
-        $("#apply-friend-verify-tab").children("img").attr("src",photo);
+        var photo = $(this).parent().children(".srl-item-avatar").children("img").attr("src");
+        var name = $(this).parent().children(".srl-item-avatar").children("img").attr("alt");
+        $("#apply-friend-verify-tab").find("img").attr("src",photo);
         $("#tb-name-user-name").html(name);
-        var fid = $(this).parent(".srl-item").attr("data-account");
-        alert(fid+name+photo);
+        var fid = $(this).parent().attr("data-account");
         $("#afv-submit").attr("data-account",fid);
         $("#apply-friend-verify-tab").show();
     });
